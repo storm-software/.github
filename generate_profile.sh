398a4e6f8fd20adfec669fc8d563c53592d585ff
Patrick Sullivan <patrick.joseph.sullivan@protonmail.com>
2024-05-01 06:43:11 -0400

    fix(monorepo): Resolve formatting issues in generate script

--- generate_profile.sh
+++ generate_profile.sh
@@ -20,14 +20,18 @@ generate_repo_list() {
     local repo_hyperlink="<a href=\"https://github.com/$repo_name\">$repo_name</a>"
     local stars="<a href=\"https://github.com/$repo_name/stargazers\"><img alt=\"GitHub Repo stars\" src=\"https://img.shields.io/github/stars/$repo_name?style=for-the-badge&color=1fb2a6\"/></a>"
 
-    echo "### $index. $repo_base_name" >>profile/README.md
-    echo "URL: $repo_hyperlink" >>profile/README.md
-    echo "Description: $description" >>profile/README.md
+    echo "### $index. [$repo_base_name]($repo_hyperlink)" >>profile/README.md
+    echo "" >>profile/README.md
+    echo "$description" >>profile/README.md
+    echo "" >>profile/README.md
     echo "$stars" >>profile/README.md
+    echo "" >>profile/README.md
+    echo "" >>profile/README.md
 }
 
 # Start README file with header
 echo "<div align=\"center\"><img src=\"https://pub-761b436209f44a4d886487c917806c08.r2.dev/storm-banner.gif\" alt=\"Storm Software\" width=\"100%\"/></div>" >profile/README.md
+echo "" >>profile/README.md
 echo "<br /><div align=\"center\"><b><a href=\"https://stormsoftware.com\" target=\"_blank\">Website</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://stormsoftware.com/contact\" target=\"_blank\">Contact</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://discord.gg/MQ6YVzakM5\" target=\"_blank\">Discord</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://www.linkedin.com/in/pat-sullivan-dev/\" target=\"_blank\">LinkedIn</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://medium.com/storm-software\" target=\"_blank\">Medium</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://www.patreon.com/StormSoftware\" target=\"_blank\">Sponsorship</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://keybase.io/sullivanp\" target=\"_blank\">OpenPGP Key</a></b></div><br /><div align="center"><b>Fingerprint:</b> 1BD2 7192 7770 2549 F4C9 F238 E6AD C420 DA5C 4C2D</div><hr />" >>profile/README.md
 echo "" >>profile/README.md
 echo "Storm Software is an open-source software development organization and the creator of Acidic, Storm Stack, and Storm Cloud." >>profile/README.md
@@ -69,5 +73,6 @@ echo "" >>profile/README.md
 echo "## Join us" >>profile/README.md
 echo "Join us on [**Discord**](https://discord.gg/MQ6YVzakM5) to chat with the team, receive release notifications, ask questions, and get involved." >>profile/README.md
 echo "" >>profile/README.md
+echo "If this sounds interesting, and you would like to help us in creating the next generation of development tools, please reach out on our [website](https://stormsoftware.com/contact) or join our [Slack](https://join.slack.com/t/storm-software/shared_invite/zt-2gsmk04hs-i6yhK_r6urq0dkZYAwq2pA)!" >>profile/README.md
 echo "" >>profile/README.md
 echo "" >>profile/README.md
