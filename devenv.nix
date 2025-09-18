eb1343f8dddc2a5c42ea44bea7908ea8e658a075
Pat <pat@patsullivan.org>
2025-09-18 17:10:30 +0000

    feat: Added repositories and `minimal` workspace config

---
+++ devenv.nix
@@ -0,0 +1,16 @@
+{ pkgs, inputs, ... }:
+{
+  name = "storm-software/github-profile";
+
+  dotenv.enable = true;
+  dotenv.filename = [".env" ".env.local"];
+  dotenv.disableHint = true;
+
+  # https://devenv.sh/basics/
+  env.DEFAULT_LOCALE = "en_US";
+  env.DEFAULT_TIMEZONE = "America/New_York";
+
+  # https://devenv.sh/packages/
+  packages = [];
+}
+
