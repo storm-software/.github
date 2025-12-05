bd1a49a6e79e40689a04fdb371c7e04b86eb04d3
Pat <pat@patsullivan.org>
2025-12-05 01:05:59 +0000

    feat: Added support for dark/light modes

--- generate_profile.sh
+++ generate_profile.sh
@@ -29,7 +29,7 @@ generate_repo_list() {
 }
 
 # Start README file with header
-echo "<div align=\"center\"><img src=\"https://public.storm-cdn.com/storm-software/banner-1280x640-optimized.gif\" alt=\"Storm Software\" width=\"100%\"/></div>" >profile/README.md
+echo "<div align=\"center\"><picture><source media=\"(prefers-color-scheme: dark)\" srcset=\"https://public.storm-cdn.com/storm-software/banner-1280x640-dark-optimized.gif\"><source media=\"(prefers-color-scheme: light)\" srcset=\"https://public.storm-cdn.com/storm-software/banner-1280x640-light-optimized.gif\"><img src=\"https://public.storm-cdn.com/storm-software/banner-1280x640-dark-optimized.gif\" width=\"100%\" alt=\"Storm Software\" /></picture></div>" >profile/README.md
 echo "" >>profile/README.md
 echo "<br /><div align=\"center\"><b><a href=\"https://stormsoftware.com\" target=\"_blank\">Website</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://stormsoftware.com/contact\" target=\"_blank\">Contact</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://discord.gg/MQ6YVzakM5\" target=\"_blank\">Discord</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://t.me/storm_software\" target=\"_blank\">Telegram</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://medium.com/storm-software\" target=\"_blank\">Medium</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://www.patreon.com/StormSoftware\" target=\"_blank\">Sponsorship</a><span>&nbsp;&nbsp;•&nbsp;&nbsp;</span><a href=\"https://keybase.io/sullivanp\" target=\"_blank\">OpenPGP Key</a></b></div><br /><div align="center"><b>Fingerprint:</b> 1BD2 7192 7770 2549 F4C9 F238 E6AD C420 DA5C 4C2D</div><hr />" >>profile/README.md
 echo "" >>profile/README.md
